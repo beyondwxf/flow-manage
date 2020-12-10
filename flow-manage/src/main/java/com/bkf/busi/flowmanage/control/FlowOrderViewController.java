178125e2902894150d57bf0294062e1c781cc406
wangxuefei <wangxuefei@staff.hexun.com>
2020-12-10 20:03:35 +0800

    增加页面

---
+++ flow-manage/src/main/java/com/bkf/busi/flowmanage/control/FlowOrderViewController.java
@@ -0,0 +1,40 @@
+package com.bkf.busi.flowmanage.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ *功能描述
+ * @author wangxuefei
+ * @date 2020/12/10
+ * @param
+ * @return
+ */
+@Controller
+public class FlowOrderViewController {
+
+    @GetMapping("/list")
+    public String index() {
+
+        return "list";
+    }
+
+
+    /**
+     * 导入页面
+     * @param req
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value="to_upload",method={RequestMethod.GET,RequestMethod.POST})
+    public String toUpload(HttpServletRequest req) {
+        return ("upload_excel");
+    }
+
+}
